e616b1392b2a3395566537863150cebf75293351
Gabriel Closato <gabrielclosato@gmail.com>
2022-09-13 19:28:14 -0300

    Terraform Infra

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -25,7 +25,7 @@ resource "aws_s3_bucket_object" "codigo_spark" {
   bucket = aws_s3_bucket.datalake.id
   key    = "emr-code/pyspark/job_spark_from_tf.py"
   acl    = "private"
-  source = "../job_spark.py"
+  source = "scripts/job_spark.py"
   etag   = filemd5("scripts/job_spark.py")
 }
 
